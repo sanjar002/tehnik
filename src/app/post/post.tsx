33b4b6c6e9cb5f5cea31b8fd5c243d7a6509f49d
Sanjar <126055171+sanjar002@users.noreply.github.com>
2024-05-06 15:26:36 +0300

    immit

---
+++ src/app/post/post.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const post = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+export default post
